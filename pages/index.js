24958871dbc32c58f05ac67b3026a65a7f248f84
Th0masCat <catsaredogstoo@gmail.com>
2023-05-12 20:22:38 +0530

    finished components

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,21 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import Filters from '../components/filters'
 import Components from '../components/components'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-
-
 export default function Home() {
+  const [data, setData] = useState([])
+
   useEffect(() => {
-    axios.get('https://api.spacexdata.com/v3/launches?limit=100')
-    .then(res => {
-      console.log(res.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    axios.get('https://api.spacexdata.com/v3/launches?limit=10')
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }, [])
 
 
@@ -28,9 +29,15 @@ export default function Home() {
         <h1 className='text-3xl font-bold'>SpaceX Launch</h1>
         <div className='flex'>
           <Filters />
-          <Components name='haha' launchYear="2191" launchSuccess="haha" landingSuccess='haha'/>
+          {
+            data.map((item, index) => {
+              return (
+                <Components key={index} name={item.mission_name} num={item.flight_number} img={item.links.mission_patch} mission_id = {item.mission_id} launchYear={item.launch_year} launchSuccess={item.launch_success} landingSuccess={item.rocket.first_stage.cores[0].land_success} />
+              )
+            }
+            )}
         </div>
-       
+
 
       </section>
     </div>
